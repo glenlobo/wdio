cee34a1441b3fadcba5a130ab68134ce9184e450
Trinity_Got_it <60222334+glenlobo@users.noreply.github.com>
2025-11-02 22:09:59 +1030

    updated wdio to comment unnecessary code

--- wdio.conf.js
+++ wdio.conf.js
@@ -297,14 +297,9 @@ export const config = {
    * @param {object}  test             test object
    * @param {object}  context          scope object the test was executed with
    * @param {object}  result          scope object the test was executed with
-   * @param {Error}   result.error     error object in case the test fails, otherwise `undefined`
-   * @param {*}       result.result    return object of test function
-   * @param {number}  result.duration  duration of test
-   * @param {boolean} result.passed    true if test has passed, otherwise false
-   * @param {object}  result.retries   information about spec related retries, e.g. `{ attempts: 0, limit: 0 }`
    */
   afterTest: async function (test, context, result) {
-    const {error, result: returnValue, duration, passed, retries} = result;
+    const {error, result: passed} = result;
     if (!passed || error) {
       const screenshot = await browser.takeScreenshot();
       const name = test.title + '- Test Failed';
@@ -313,65 +308,65 @@ export const config = {
     }
   },
 
-  /**
-   * Hook that gets executed after the suite has ended
-   * @param {object} suite suite details
-   */
+  // /**
+  //  * Hook that gets executed after the suite has ended
+  //  * @param {object} suite suite details
+  //  */
   // afterSuite: function (suite) {
   // },
-  /**
-   * Runs after a WebdriverIO command gets executed
-   * @param {string} commandName hook command name
-   * @param {Array} args arguments that command would receive
-   * @param {number} result 0 - command success, 1 - command error
-   * @param {object} error error object if any
-   */
+  // /**
+  //  * Runs after a WebdriverIO command gets executed
+  //  * @param {string} commandName hook command name
+  //  * @param {Array} args arguments that command would receive
+  //  * @param {number} result 0 - command success, 1 - command error
+  //  * @param {object} error error object if any
+  //  */
   // afterCommand: function (commandName, args, result, error) {
   // },
-  /**
-   * Gets executed after all tests are done. You still have access to all global variables from
-   * the test.
-   * @param {number} result 0 - test pass, 1 - test fail
-   * @param {Array.<Object>} capabilities list of capabilities details
-   * @param {Array.<String>} specs List of spec file paths that ran
-   */
+  // /**
+  //  * Gets executed after all tests are done. You still have access to all global variables from
+  //  * the test.
+  //  * @param {number} result 0 - test pass, 1 - test fail
+  //  * @param {Array.<Object>} capabilities list of capabilities details
+  //  * @param {Array.<String>} specs List of spec file paths that ran
+  //  */
   // after: function (result, capabilities, specs) {
   // },
-  /**
-   * Gets executed right after terminating the webdriver session.
-   * @param {object} config wdio configuration object
-   * @param {Array.<Object>} capabilities list of capabilities details
-   * @param {Array.<String>} specs List of spec file paths that ran
-   */
+  // /**
+  //  * Gets executed right after terminating the webdriver session.
+  //  * @param {object} config wdio configuration object
+  //  * @param {Array.<Object>} capabilities list of capabilities details
+  //  * @param {Array.<String>} specs List of spec file paths that ran
+  //  */
   // afterSession: function (config, capabilities, specs) {
   // },
-  /**
-   * Gets executed after all workers got shut down and the process is about to exit. An error
-   * thrown in the onComplete hook will result in the test run failing.
-   * @param {object} exitCode 0 - success, 1 - fail
-   * @param {object} config wdio configuration object
-   * @param {Array.<Object>} capabilities list of capabilities details
-   * @param {<Object>} results object containing test results
-   */
+  // /**
+  //  * Gets executed after all workers got shut down and the process is about to exit. An error
+  //  * thrown in the onComplete hook will result in the test run failing.
+  //  * @param {object} exitCode 0 - success, 1 - fail
+  //  * @param {object} config wdio configuration object
+  //  * @param {Array.<Object>} capabilities list of capabilities details
+  //  * @param {<Object>} results object containing test results
+  //  */
   // onComplete: function(exitCode, config, capabilities, results) {
   // },
-  /**
-   * Gets executed when a refresh happens.
-   * @param {string} oldSessionId session ID of the old session
-   * @param {string} newSessionId session ID of the new session
-   */
+  // /**
+  //  * Gets executed when a refresh happens.
+  //  * @param {string} oldSessionId session ID of the old session
+  //  * @param {string} newSessionId session ID of the new session
+  //  */
   // onReload: function(oldSessionId, newSessionId) {
   // }
-  /**
-   * Hook that gets executed before a WebdriverIO assertion happens.
-   * @param {object} params information about the assertion to be executed
-   */
+  // /**
+  //  * Hook that gets executed before a WebdriverIO assertion happens.
+  //  * @param {object} params information about the assertion to be executed
+  //  */
   // beforeAssertion: function(params) {
   // }
-  /**
-   * Hook that gets executed after a WebdriverIO assertion happened.
-   * @param {object} params information about the assertion that was executed, including its results
-   */
+  // **
+  //  * Hook that gets executed after a WebdriverIO assertion happened.
+  //  * @param {object} params information about the assertion that was executed, including its results
+  //  *
   // afterAssertion: function(params) {
   // }
 
